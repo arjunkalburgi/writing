ae3fc2aad2a1daf8622b84ae898491876fa02551
Arjun Kalburgi <askalburgi@gmail.com>
2020-03-21 14:32:38 -0400

    redoing siteurl for writing slug link

--- src/.temp/config.js
+++ src/.temp/config.js
@@ -1,8 +1,8 @@
 export default {
   "trailingSlash": true,
   "titleTemplate": "%s - Arjun Kalburgi Blog",
-  "siteUrl": "https://www.arjunkalburgi.com",
-  "pathPrefix": "/writing",
+  "siteUrl": "https://www.arjunkalburgi.com/writing",
+  "pathPrefix": "/",
   "version": "0.7.13",
   "catchLinks": true
 }
