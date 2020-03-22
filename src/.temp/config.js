6a1190b70d4f65d744f2a0ec59e202f6aee72f5b
Arjun Kalburgi <askalburgi@gmail.com>
2020-03-21 23:00:06 -0400

    putting prefix back

--- src/.temp/config.js
+++ src/.temp/config.js
@@ -1,7 +1,8 @@
 export default {
   "trailingSlash": true,
+  "pathPrefix": "/writing",
   "titleTemplate": "%s - Arjun Kalburgi Blog",
-  "siteUrl": "https://www.arjunkalburgi.com/writing",
+  "siteUrl": "https://www.arjunkalburgi.com",
   "version": "0.7.13",
   "catchLinks": true
 }
